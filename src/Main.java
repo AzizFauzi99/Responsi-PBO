4d7a3e926efca2f100be05d515fbe4a44469d2c0
Aziz Fatih Fauzi <92005833+AzizFauzi99@users.noreply.github.com>
2022-05-31 12:17:21 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ import View.AslabView;
 public class Main {
     public static void main(String[] args) {
         AslabView av = new AslabView();
-        av.dispose();
         AslabModel am = new AslabModel();
         AslabController ac = new AslabController(av, am);
     }
